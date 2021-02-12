ed3dd2ba2e010689eec64d47acbed202d9ea2b72
Matheus Martino <matheus.martino97@gmail.com>
2021-02-12 03:59:00 -0300

    Fixing css.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Pokedex from './Pokedex'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Pokédex</h1>
+      <Pokedex/>
+    </div>
+    
+  );
+}
+
+export default App;
